88e548f78507778ee70ce8b0ab648ea86f2f600d
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2024-10-07 13:54:14 +0530

    Update settings.py

--- lfproject/settings.py
+++ lfproject/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://lost-and-found-crce-ndeo6k8ex-soham1903s-projects.vercel.app",
+    "https://lost-and-found-crce-9is297rcy-soham1903s-projects.vercel.app/",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
